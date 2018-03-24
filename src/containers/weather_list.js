9bf87f31fc293f20b62de627ac5dc68ea6b32133
Bao Nguyen The Vinh <bao.nguyen150028@gmail.com>
2018-03-24 20:46:49 +0900

    Finish render chart

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 class WeatherList extends Component {
     renderWeather(cityData) {
+        console.log(cityData);
         const name = cityData.city.name;
+        const temps = cityData.list.map(weather => weather.main.temp)
+
         return(
             <tr key={name}>
                 <td>{name}</td>
+                <td> 
+                    <Sparklines height={120} width={180} data={temps}>
+                        <SparklinesLine color="red" />
+                    </Sparklines>
+                </td>
             </tr>
         );
     }
@@ -22,7 +31,7 @@ class WeatherList extends Component {
                     </tr>
                 </thead>
                 <tbody>
-                    {this.props.weather.map(this.renderWeather)}
+                    {this.props.weather.map((cityData) => this.renderWeather(cityData))}
                 </tbody>
             </table>
         )
